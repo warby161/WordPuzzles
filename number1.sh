16aa00035d0856c45263956006e1ad8400bcd245
Nick Warabak <nick.warabak@gmail.com>
2016-04-07 23:09:23 -0400

    Solution for number 1 - single pattern at a time only

---
+++ number1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter file name: " file
+read -p "Enter search pattern: " pattern
+
+if [ -f "$file" ]
+then
+    result=$(grep -i "$pattern" "$file")
+    echo "$result"
+else
+    echo "File not found" 
+fi
